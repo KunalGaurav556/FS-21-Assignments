70ae0eeb1f97127a6053dc1a147f848ea3a6d404
KunalGaurav556 <kunalgauravhk556@gmail.com>
2024-08-06 16:08:15 +0530

    Adding Random Color Generator

---
+++ Javascript/Random Color Generator/script.js
@@ -0,0 +1,40 @@
+let main = document.getElementById("main");
+
+for (let i = 0; i < 100; i++) {
+    // let div = document.createElement("div");
+    let div = `<div class="color_container"></div>`
+    // main.appendChild(div);
+    main.innerHTML += div;
+}
+// console.log(main);
+
+function randomColor() {
+    let number = "1234567890abcdef";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+        let randColr = Math.floor(Math.random() * number.length);
+        color = color + number[randColr];
+        
+    }
+    return color
+    // console.log(color)
+}
+
+function generateColor() {
+    let colorConatainer = document.querySelectorAll(".color_container");
+    console.log(colorConatainer);
+
+    colorConatainer.forEach((ele)=>{
+        let newColor = randomColor();
+        ele.style.background = newColor;
+        ele.innerHTML = newColor;
+        ele.style.width = "200px";
+        ele.style.height = "150px";
+        ele.style.borderRadius = "20px"
+        // ele.stye.display = "flex";
+    })
+   
+}
+
+
+window.addEventListener("load", generateColor);
