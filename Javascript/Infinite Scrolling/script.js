8d35ac188247236d149eff24e8f9556a3adbeb22
KunalGaurav556 <kunalgauravhk556@gmail.com>
2024-08-22 00:06:40 +0530

    Added some changes in ininte project

--- Javascript/Infinite Scrolling/script.js
+++ Javascript/Infinite Scrolling/script.js
@@ -1,36 +1,30 @@
-let key = "hou5apMau9Ai4Go9TDuVjc-n_kyOtuK9_a5_gn1t7Fg"; 
+let key = "xR2R2sC6O_PPuLFjzR7U_3Ea7NAo7uUEAiC6dIfBjWM";
 let box = document.querySelector(".box");
 let apiUrl = `https://api.unsplash.com/photos/random/?client_id=${key}&count=30`;
 console.log(apiUrl)
 
-function updataUI(data){
-   data.forEach(photo => {
-    let img = document.createElement("img");
-    img.src = photo.urls.small;
-    img.style.height = "300px"
-    img.style.width = "250px"
-    
-    box.appendChild(img);
-    // console.log(img);
+function updataUI(data) {
+    data.forEach(photo => {
+        let img = document.createElement("img");
+        img.src = photo.urls.small;
+        img.style.height = "300px";
+        img.style.width = "250px";
 
-   });
-
+        box.appendChild(img);
+        // console.log(box);
+    });
 }
-//    updataUI();
 
 async function fetchData() {
     let response = await fetch(apiUrl);
     let data = await response.json();
     console.log(data);
-    // return data;
     updataUI(data);
-    
 }
 fetchData();
 
-window.addEventListener("scroll" , function(){
-    if(this.window.scrollY+this.window.innerHeight >=this.document.body.offsetHeight){
+window.addEventListener("scroll", function () {
+    if (window.scrollY + window.innerHeight >= document.body.offsetHeight) {
         fetchData();
-        // updataUI()
     }
-})
+});
