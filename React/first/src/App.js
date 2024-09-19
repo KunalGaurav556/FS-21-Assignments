4687fba0a19308760c3ecc0ee5e9e5d7d69565f4
KunalGaurav556 <kunalgauravhk556@gmail.com>
2024-09-19 23:11:50 +0530

    Added react

---
+++ React/first/src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+// import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <h1>
+        Learn web development
+        </h1>
+        <p>
+        Welcome to the MDN learning area. This set of articles aims to guide
+        complete beginners to web development with all that they need to start
+        coding websites. The aim of this area of MDN is not to take you from
+        "beginner" to "expert" but to take you from "beginner" to "comfortable."
+        From there, you should be able to start making your way, learning from
+        the rest of MDN, and other intermediate to advanced resources that
+        assume a lot of previous knowledge. If you are a complete beginner, web
+        development can be challenging — we will hold your hand and provide
+        enough detail for you to feel comfortable and learn the topics properly.
+        You should feel at home whether you are a student learning web
+        development (on your own or as part of a class), a teacher looking for
+        class materials, a hobbyist, or someone who just wants to understand
+        more about how web technologies work.
+      </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
